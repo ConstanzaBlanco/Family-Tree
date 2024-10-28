da885732d7de7f3e8b2b9565ba466eaf9b217825
ConstanzaBlanco <constanza.blanco@correo.ucu.edu.uy>
2024-10-28 11:55:50 -0300

    Clase Person

---
+++ src/Library/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace Library;
+
+public class Person
+{
+    private int Edad { get; set; }
+    private string Name { get; set; }
+
+    public Person(int edad, string name)
+    {
+        this.Edad = edad;
+        this.Name = name;
+    }
+
+    public int GetAge()
+    {
+        return this.Edad;
+    }
+
+    public string GetName()
+    {
+        return this.Name;
+    }
+}
